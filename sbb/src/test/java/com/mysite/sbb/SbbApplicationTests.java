b79cc7a6c89f8e6a2bb1da3d56723182f103e839
jissssoo1717 <jisoolee7101@gmail.com>
2023-05-16 19:13:01 +0900

    Create Repos and JpaTestCode for Repos

--- sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,13 +1,27 @@
 package com.mysite.sbb;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SbbApplicationTests {
 
+	// DI => 테스트 코드의 경우 생성자를 통한 객체 주입 불가능, @Autowired 이용
+	@Autowired
+	private QuestionRepository questionRepository;
+	
 	@Test
-	void contextLoads() {
+	void testJpa() {
+		List<Question> all = this.questionRepository.findAll();
+		assertEquals(2, all.size());
+		
+		Question q = all.get(0);
+		assertEquals("엥", q.getSubject());
 	}
 
 }
