4613e3559d92c1e74738431ccd455fec7e69a194
jissssoo1717 <jisoolee7101@gmail.com>
2023-05-23 15:53:37 +0900

    질문 게시판 기능 추가

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -5,22 +5,33 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 
-
+@RequestMapping("/question")
 @RequiredArgsConstructor// final이 붙은 속성을 포함하는 생성자를 자동으로 생성(DI)
 @Controller
 public class QuestionController {
 	
 	private final QuestionService questionService;
 
-	@GetMapping("/question/list")
+	@GetMapping("/list")
 	public String list(Model model) {
 		
 		List<Question> questionList = this.questionService.getList();
 		model.addAttribute("questionList", questionList);
 		
 		return "question_list";
+	}
+	
+	@GetMapping("/detail/{id}")
+	public String detail(Model model, @PathVariable("id") Integer id) {
+		
+		Question question = this.questionService.getQuestion(id);
+		model.addAttribute("question", question);
+		
+		return "question_detail";
 	}
 }
